f0c679a25ab94c277251878f2b1264cbecbb4d80
kpengfang <yykpf1206@163.com>
2016-05-22 03:25:37 +0800

    20160522

--- js/index.js
+++ js/index.js
@@ -1,52 +1,47 @@
-/**
- * Created by xiao lei on 2016/5/11.
- */
 $(document).on('touchmove',function(ev){
     ev.preventDefault();
 });
 $(function(){
-    //处理高度的兼容性问题；
+    //解决各设备高度适配问题
     var viewHeight=$(window).height();
-    //var $li=$('#list').find('>li');
-    //var $li=$('#list').children('li');
-    var $li=$('#list>li');
+    var $li=$('#list').find('>li');
     $('#main').css('height',viewHeight);
     slidePage();
     function slidePage(){
-        var touchY=0;
+        var startY=null;
         var step=1/4;
         var nowIndex=0;
         var nextorprevIndex=0;
         var bOk=true;
+        //绑定touchstart事件
         $li.on('touchstart',function(ev){
             if(bOk==false) return;
             bOk=false;
+            var touch=ev.originalEvent.touches[0];
+            startY=touch.pageY;
             nowIndex=$(this).index();
-            //touch事件
-            var touch=ev.originalEvent.changedTouches[0];
-            touchY=touch.pageY;//保留按下的位置；
-            //Math.abs(touch.pageY-touchY)/viewHeight*step  0~0.25 1~0.75
             $li.on('touchmove.move',function(ev){
-                var touch=ev.originalEvent.changedTouches[0];
+                var touch=ev.originalEvent.touches[0];
                 $(this).siblings('li').hide();
-                if(touch.pageY<touchY){//up
+                //首先会进行上下划动的判断
+                //缩放公式：1-Math.abs(touch.pageY-startY)/viewHeight*step 0~0.25 1~0.75
+                if(touch.pageY<startY){
                     nextorprevIndex=nowIndex==$li.length-1?0:nowIndex+1;
-                    $li.eq(nextorprevIndex).css('transform','translate(0,'+(viewHeight+touch.pageY-touchY)+'px)');
-                }else if(touch.pageY>touchY){//down
+                    $li.eq(nextorprevIndex).css('transform','translate(0,'+(viewHeight+touch.pageY-startY)+'px)')
+                }else if(touch.pageY>startY){//down
                     nextorprevIndex=nowIndex==0?$li.length-1:nowIndex-1;
-                    $li.eq(nextorprevIndex).css('transform','translate(0,'+(-viewHeight+touch.pageY-touchY)+'px)');
+                    $li.eq(nextorprevIndex).css('transform','translate(0,'+(-viewHeight+touch.pageY-startY)+'px)')
                 }else{
                     bOk=true;
                 }
-                //实现位移： transform-translate; 实现缩放： transform-scale
-                $(this).css('transform','translate(0,'+(touch.pageY-touchY)*step+'px) scale('+(1-Math.abs(touch.pageY-touchY)/viewHeight*step)+')');
+                $(this).css('transform','translate(0,'+(touch.pageY-startY)*step+'px) scale('+(1-Math.abs(touch.pageY-startY)/viewHeight*step)+')');
                 $li.eq(nextorprevIndex).addClass('zIndex').show();
-            })
+            });
             $li.on('touchend.move',function(ev){
-                var touch=ev.originalEvent.changedTouches[0];
-                if(touch.pageY<touchY) {//up
+               var touch=ev.originalEvent.changedTouches[0];
+                if(touch.pageY<startY) {//up
                     $(this).css('transform','translate(0,'+(-viewHeight*step)+'px) scale('+(1-step)+')')
-                }else if(touch.pageY>touchY) {//down
+                }else if(touch.pageY>startY) {//down
                     $(this).css('transform','translate(0,'+(viewHeight*step)+'px) scale('+(1-step)+')')
                 }else{
                     bOk=true;
@@ -56,21 +51,17 @@ $(function(){
                 $li.eq(nowIndex).css('transition','.3s');
                 $li.off('.move');
             })
-
-        })
+        });
         $li.on('transitionend webkitTransitionend',function(ev){
-            if(!$li.is(ev.target)){
-                return;
-            }
+            if(!$li.is(ev.target)) return;
             resetFn();
-        })
+        });
         function resetFn(){
-            $li.css('transition','');
             $li.css('transform','');
-            $li.eq(nextorprevIndex).siblings('li').hide();
-            $li.eq(nextorprevIndex).removeClass('zIndex');
+            $li.css('transition','');
+            $li.eq(nextorprevIndex).removeClass('zIndex').siblings('li').hide();
             bOk=true;
         }
     }
+});
 
-})
